9a3a712e961da491c5feaa414a8ad502bff97001
EvidenceEkanem <ekanemevidence@gmail.com>
2019-09-16 21:54:44 +0100

    did some finishing touches for both front end and backend

--- index.php
+++ index.php
@@ -6,18 +6,20 @@
 	<title>Team Aces Log-in Page</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 	
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="wrap-login100 p-t-85 p-b-20">
+			<div class="wrap-login100">
 					<?php 
 					if (isset($_SESSION['errors'])){
 						foreach ($_SESSION['errors'] as $error) {
-							echo "<h3>".$error."</h3>";
+							echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
 							unset($errors);
 						}
 						unset($_SESSION['errors']);
@@ -27,19 +29,19 @@
 				action="./scripts/login.php"
             	method="post"
 				class="login100-form validate-form">
-					<span class="login100-form-title p-b-70">
+					<span class="login100-form-title pb-1">
 						Welcome
 					</span>
 					<span class="login100-form-avatar">
 						<img src="images/avatar1.jpg" alt="AVATAR">
 					</span>
 
-					<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "Enter username">
+					<div class="wrap-input100 my-4">
 						<input class="input100" type="text" name="username">
 						<span class="focus-input100" data-placeholder="Username"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter password">
+					<div class="wrap-input100 mb-3">
 						<input class="input100" type="password" name="password">
 						<span class="focus-input100" data-placeholder="Password"></span>
 					</div>
@@ -49,28 +51,6 @@
 							Login
 						</button>
 					</div>
-
-					<ul class="login-more p-t-190">
-						<li class="m-b-8">
-							<span class="txt1">
-								Forgot
-							</span>
-
-							<a href="#" class="txt2">
-								Username / Password?
-							</a>
-						</li>
-
-						<li>
-							<span class="txt1">
-								Don’t have an account?
-							</span>
-
-							<a href="#" class="txt2">
-								Sign up
-							</a>
-						</li>
-					</ul>
 				</form>
 			</div>
 		</div>
@@ -79,5 +59,7 @@
 
 	<div id="dropDownSelect1"></div>
 
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
 </body>
 </html>
